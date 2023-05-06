f6a29a97af7a7d8f0ed20d9dc7624f8cdfaf0178
aktnk <13390370+aktnk@users.noreply.github.com>
2023-05-06 23:51:25 +0900

    Revert "refine install script"
    This reverts commit 3b1fc214bc2436bfdfad6958b723b697deee2f65.

--- source/nginx-php-inst.sh
+++ source/nginx-php-inst.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-yum update -y
-timedatectl set-timezone Asia/Tokyo
-amazon-linux-extras install -y nginx1 php8.2
-echo '<?php echo "Request from IP: ".$_SERVER["REMOTE_ADDR"]." at ".date(DATE_ATOM); ?>' | tee /usr/share/nginx/html/index.php
-sed -i -e "s/^expose_php = On/expose_php = Off/" -e "s/^;date.timezone =/date.timezone = Asia\/Tokyo/" /etc/php.ini
-echo 'server_tokens off;' | teee /etc/nginx/conf.d/default.conf
-systemctl start php-fpm
-systemctl start nginx
-systemctl enable php-fpm
-systemctl enable nginx
+sudo yum update -y
+sudo timedatectl set-timezone Asia/Tokyo
+sudo amazon-linux-extras install -y nginx1
+sudo amazon-linux-extras install -y php8.2
+echo '<?php echo "Request from IP: ".$_SERVER["REMOTE_ADDR"]." at ".date(DATE_ATOM); ?>' | sudo tee /usr/share/nginx/html/index.php
+sudo sed -i -e "s/^expose_php = On/expose_php = Off/" -e "s/^;date.timezone =/date.timezone = Asia\/Tokyo/" /etc/php.ini
+echo 'server_tokens off;' | sudo tee /etc/nginx/conf.d/default.conf
+sudo systemctl start php-fpm
+sudo systemctl start nginx
+sudo systemctl enable php-fpm
+sudo systemctl enable nginx
